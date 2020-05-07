923e69378818ccd51ec69040212ef2b281da449b
Ronald Lima <ronaldazevedolima@gmail.com>
2020-05-07 17:15:52 -0300

    cc

--- src/average.js
+++ src/average.js
@@ -15,11 +15,11 @@
 const average = (arr) => {
   if (arr.length === 0) return undefined;
   let soma = 0;
-  for( let i = 0; i < arr.length; i++) {
-      if (typeof arr[i] === 'string') return undefined
-      soma += arr[i];
-    }
-    return Math.round(soma / arr.length);
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] === 'string') return undefined;
+    soma += arr[i];
+  }
+  return Math.round(soma / arr.length);
 };
 
 module.exports = average;

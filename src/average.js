fede2f61c39e02973fc4f0986a33391729713d1d
Leandro Marten <leandromarten1@gmail.com>
2020-05-07 15:49:37 -0300

    install npm

--- src/average.js
+++ src/average.js
@@ -14,7 +14,17 @@
 
 const average = (arr) => {
 
-  
+  if(arr.length == 0){
+    return undefined;
+    break;
+  }
+  const totalNum = arr.length;
+  let soma = 0;
+  for (let i=0; i<arr.length; i+=1){
+    if(typeof(arr[i]) == 'string') return undefined;
+    soma = soma + arr[i];
+  }
+  return Math.round(media/total);
 
 };
 

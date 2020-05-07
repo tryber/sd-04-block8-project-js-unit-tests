0f17a446b84b0547ac59f688a63f6b3c85038576
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-05-07 16:21:28 -0300

    Primeiro requisito

--- src/average.js
+++ src/average.js
@@ -12,7 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-// add your implementation here
+const average = (arr) => {
+  let sum = 0;
+  if (arr.length === 0) return undefined;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+    if (typeof arr[i] !== 'number') return undefined;
+  }
+  return (Math.round(sum / arr.length));
+};
 
 module.exports = average;

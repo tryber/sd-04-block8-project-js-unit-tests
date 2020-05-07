dc3c350512c7dc86fd74d05f8855e1a33d35aeca
Jorge Osiro <jojimurakabe@gmail.com>
2020-05-07 19:10:24 -0300

    fixing

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,29 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(productDetails('mouse', 'teclado'),[
+      {
+        name: 'mouse',
+        details: {
+          productId: 'mouse123'
+        }
+      },
+      {
+        name: 'teclado',
+        details: {
+          productId: 'teclado123'
+        }
+      }
+    ])
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.notDeepStrictEqual(productDetails('mouse', 'teclado'), undefined);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('mouse', 'teclado'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('mouse', 'teclado'), undefined);
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });

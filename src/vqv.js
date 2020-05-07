7775d6f61e9aee323da46aee6000032ff9b5a2b4
AliceAAtalla <aliceatalla36@gmail.com>
2020-05-07 17:50:29 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'undefined' || typeof idade !== 'undefined') {
+    return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  }
+  return undefined;
+};
 
 module.exports = vqv;

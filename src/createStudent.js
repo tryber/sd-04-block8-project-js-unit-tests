f160ccf2cb8250cad46abfb4f6f6c1f68626a4dd
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-05-07 18:47:53 -0300

    5th requirement

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  return estudante = {
+    name: name,
+    feedback() { return 'Eita pessoa boa!' }
+  }
+  
+};
 
 module.exports = createStudent;

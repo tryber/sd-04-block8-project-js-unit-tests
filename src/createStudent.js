a852999fc0a6e8e7fe654b6be42269f41e463a52
Lucas Ribas <lucas@mundoribas.com.br>
2020-05-07 19:01:40 -0300

    Exercise Create Student - Ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (student) => {
+  const objeto = {
+    name: student,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return objeto
+};
 
 module.exports = createStudent;

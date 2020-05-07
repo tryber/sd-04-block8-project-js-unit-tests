5cb3bbb38c821125a432fe77e6d21ff9587e330c
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-07 15:52:30 -0300

    average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  if (arr.length === 0) return undefined;
+  let result = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== 'number') return undefined;
+    result = (result + arr[i]) / (i + 1);
+  }
+  return Math.round(result);
+};
 
 module.exports = average;

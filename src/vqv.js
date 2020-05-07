0e6fbaafd895add12ba440fe611b123a5e2d64fb
Lucas Ribas <lucas@mundoribas.com.br>
2020-05-07 17:27:29 -0300

    CC bug Fixed

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,7 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome !== 'string' || typeof idade !== 'number') { 
+  if (typeof nome !== 'string' || typeof idade !== 'number') {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;

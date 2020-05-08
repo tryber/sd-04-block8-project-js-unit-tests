c19f269fcc662f0808a20e6e0ab41fd0bf3d6682
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-05-08 18:25:10 -0300

    Update createStudent.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const student = {name: name + ", o Lobo Solitário", feedback:"Eita pessoa boa!"}
+  return student
+};
 
 module.exports = createStudent;

32c3d3372062f742eb4ae68dcdd74d55268c0ba3
frederico-campello <fredfmc33@gmail.com>
2020-05-08 20:51:07 -0300

    Iniciando o projeto

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,10 +18,7 @@
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
+    myArray.push(counter, 2, 3);
   }
   return myArray;
 };

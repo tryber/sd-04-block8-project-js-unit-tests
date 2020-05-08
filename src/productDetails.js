260e1e942799e2cbc53bf4a8f7885a492e86fa18
Eder Sena <eder.sena@live.com>
2020-05-08 01:31:51 -0300

    6 - Implemente os casos de teste no arquivo tests/productDetails.spec.js

--- src/productDetails.js
+++ src/productDetails.js
@@ -1,6 +1,11 @@
+/* eslint-disable max-len*/
+/* eslint-disable no-unused-vars */
+
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
+
 /*
-  Dadas duas strings que representam nomes de produtos,
-  retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
+  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
 
   Parâmetros:
   - Uma string;
@@ -22,21 +27,28 @@
       }
     }
   ]
+
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-const productDetails = (firstProduct, secondProduct) => [
-  {
-    name: firstProduct,
-    details: {
-      productId: `${firstProduct}123`,
-    },
-  },
-  {
-    name: secondProduct,
-    details: {
-      productId: `${secondProduct}123`,
-    },
-  },
-];
-
-module.exports = productDetails;
+describe('#productDetails', () => {
+  it('tests the function has the correct behaviour', () => {
+    //assert.fail();
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    assert.equal(Array.isArray(productDetails()), true)
+    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.equal(productDetails('Alcool gel', 'Máscara').length, 2)
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.equal(typeof(productDetails('Alcool gel', 'Máscara')), 'object')
+    // Teste que os dois objetos são diferentes entre si.
+    const iguais = productDetails('Alcool gel', 'Máscara')[0].name === productDetails('Alcool gel', 'Máscara')[1].name;
+    assert.equal(iguais, false)
+    // (Difícil) Teste que os dois productIds terminam com 123.
+    const product0 = productDetails('Alcool gel', 'Máscara')[0].details.productId;
+    const product1 = productDetails('Alcool gel', 'Máscara')[1].details.productId;
+    const p1 = product0.substring(product0.length -3 );
+    const p2 = product1.substring(product1.length -3 );
+    assert.equal(p1 === p2, true);
+  });
+});

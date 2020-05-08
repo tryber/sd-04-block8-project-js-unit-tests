e97010dee41d4aefb44500df5f14f8569971ca57
procopio420 <lucas-procopio@outlook.com>
2020-05-08 00:07:42 -0300

    finished

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let sum = 0;
+  if(!arr.length){
+    return undefined;
+  }
+  for(const element of arr){
+    if(typeof element !== 'number'){
+      return undefined;
+    } else {
+      sum+=element;
+    }
+  }
+  return Math.round(sum/arr.length);
+};
 
 module.exports = average;

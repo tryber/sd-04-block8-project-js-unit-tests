8b7c097f1fc770f5776c4222cff963100edc8ae0
Victor L <victor.lustosacp@gmail.com>
2020-05-08 16:42:07 -0300

    Parte 2 CC

--- src/numbers.js
+++ src/numbers.js
@@ -10,7 +10,7 @@
 */
 
 const numbers = (myArray) => {
-  if (myArray.length === 0) return false;
+  if( myArray.length === 0) return false;
   let zerado = 0;
   for (let i = 0; i < myArray.length; i += 1) {
     if (typeof myArray[i] !== 'number') {
@@ -20,5 +20,8 @@ const numbers = (myArray) => {
     return false;
   } return true;
 };
-
+console.log(numbers([1, 2, 3, 4, 5]));
+console.log(numbers([1, 2, '3', 4, 5]));
+console.log(numbers([1, 'a', 3]));
+console.log(numbers([' ']));
 module.exports = numbers;

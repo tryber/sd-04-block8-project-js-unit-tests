fceb11b34c370ce5cee930bbf45ad525c616a893
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-05-08 12:12:28 -0300

    Code Climate

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,10 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (let counterTwo = 2; counterTwo <= 3; counterTwo += 1) {
       myArray.push(counterTwo);
     }
   }

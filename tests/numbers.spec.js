2b7c4c270e095ac8ff82c3e9f10fe4fe7fb4984a
Victor L <victor.lustosacp@gmail.com>
2020-05-08 18:29:36 -0300

    Parte 2 CC

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -22,11 +22,11 @@ describe('#numbers', () => {
     // assert.fail();
     assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
+    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    assert.deepStrictEqual(numbers([1, 'a', 3]), false);
+    assert.deepStrictEqual(numbers([1, 'a', 3]), true);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    assert.deepStrictEqual(numbers([' ']), false);
+    assert.deepStrictEqual(numbers([' ']), true);
     // Escreva um teste em que a função recebe [' '] e retorna false
   });
 });

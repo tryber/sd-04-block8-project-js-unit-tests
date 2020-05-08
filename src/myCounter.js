626509f84761308ad54de8841321d3e3a1fcb8e8
Eder Sena <eder.sena@live.com>
2020-05-08 02:17:14 -0300

    8 - Implemente a função do arquivo src/myCounter.js

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,10 +18,10 @@
 const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
-      myArray.push(counter);
-      for (let counter = 2; counter <= 3; counter += 1) {
-          myArray.push(counter);
-      }
+    myArray.push(counter);
+    for (counter = 2; counter <= 3; counter += 1) {
+        myArray.push(counter);
+    }
   }
   return myArray;
 };

724cf13872ea50ffb822982016fe640b8d54ea7d
samueap <samueap@gmail.com>
2020-05-08 10:39:00 -0300

    fix to CC requirement 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let newCounter = 2; newCounter <= 3; newCounter += 1) {
+      myArray.push(newCounter);
     }
   }
   return myArray;

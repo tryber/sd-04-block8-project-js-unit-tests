e26f38a60d09dd5fe9ac4f1c6007f2a149e07a28
igorgbr <igorgphotoarte@gmail.com>
2020-05-07 21:23:29 -0300

    codeclimate objCalc codeclimate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,9 +20,9 @@
 */
 
 const calculator = {
-  add: (x, y) => {return x + y;},
-  mult: (x, y) => {return x * y;},
-  div: (x, y) => {return Math.floor(x / y);},
-  sub: (x, y) => {return x - y;}
+  add: (x, y) =>  x + y,
+  mult: (x, y) =>  x * y,
+  div: (x, y) =>  Math.floor(x / y),
+  sub: (x, y) =>  x - y,
 };
 module.exports = calculator;

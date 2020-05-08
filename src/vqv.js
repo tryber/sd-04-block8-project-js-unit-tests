9ea12da7f5fac96de1ca31bbdb37865567d71981
Victor L <victor.lustosacp@gmail.com>
2020-05-08 19:58:07 -0300

    Parte 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,6 +20,6 @@ const vqv = (nome, idade) => {
   console.log(`Oi, meu nome é ${nome}!
 tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`)};
+#VQV!`); };
 
 module.exports = vqv;

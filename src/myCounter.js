171320d09f7779a4f0cc440d45867aca4f81df4a
orlando-messias <programadorlando@gmail.com>
2020-05-09 13:02:48 -0300

    myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,7 +20,7 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let i = 2; i <= 3; i += 1) {
-      myArray.push(counter);
+      myArray.push(i);
     }
   }
   return myArray;

9d9c3bb8ce81a01d3ffb35aa3eaa0ebb3a9fbe60
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-05-09 08:22:06 -0300

    start from scratch

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -54,20 +54,13 @@ describe('#createMenu', () => {
     // assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // ```
-    assert.strictEqual(typeof createMenu, 'function');
-    const objetoQualquer = { food: {}, drink: {} };
-    assert.deepStrictEqual(createMenu(objetoQualquer) , { fetchMenu: objetoQualquer });
+    // assert.deepStrictEqual(createMenu('objetoQualquer'),{ fetchMenu: 'objetoQualquer' });
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, verifique que 'objetoRetornado.fetchMenu' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
-     const objetoRetornado = createMenu(objetoQualquer); objetoRetornado.fetchMenu
-
-    assert.deepStrictEqual(Object.keys(objetoRetornado.fetchMenu), ["food", "drink"]);
-    // assert.deepStrictEqual(Object.keys(createMenu({ food: {}, drink: {} }).fetchMenu), ["food", "drink"]);
-
     // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.fetchMenu // Retorno: { food: {}, drink: {}}
     // ```
@@ -75,7 +68,6 @@ describe('#createMenu', () => {
     // --------------------------------------------------------------------------------------
     // TESTE 3: Verifique que o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
     // ```
-    // assert.deepStrictEqual(,);
     // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.fetchMenu // Retorno: objetoQualquer
     // ```

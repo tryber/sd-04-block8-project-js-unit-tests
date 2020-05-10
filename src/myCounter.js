e6595b0174dabce67715e7d15a4b74f266246df4
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-05-10 18:38:26 -0300

    myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,12 +17,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (i = 0; i <= 3; i += 1) {
-    for (var counter = i; counter <= 3; counter += 1) {
-      myArray.push(counter);
-      for (var counter = 2; counter <= 3; counter += 1) {
-        myArray.push(counter);
-      }
+  for (var counter = 0; counter <= 3; counter += 1) {
+    myArray.push(counter);
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;

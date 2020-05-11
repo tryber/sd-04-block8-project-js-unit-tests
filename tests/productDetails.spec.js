78110836c988d46a1f705d72280087fbc5881a4c
wistyk <leonardoxw@gmail.com>
2020-05-10 21:44:56 -0300

    faltando o ultimo tópico do req product

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,9 +36,14 @@ describe('#productDetails', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('a', 'b')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('a', 'b').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('a, b'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepEqual(productDetails('a', 'b'));
     // (Difícil) Teste que os dois productIds terminam com 123.
+    
   });
 });

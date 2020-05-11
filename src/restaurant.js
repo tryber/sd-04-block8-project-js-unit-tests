40530728ef2fa9c34eb21542e5f0ab8f285fd104
juniomelos <you@example.com>
2020-05-11 11:46:31 -0300

    Resolved exercise 9 - Passos 5 arq restaurant.spenc e passo 3 arq restaurant - CC - 1

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,13 +71,12 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = objeto => ({
-
-    fetchMenu: objeto,
-    consumption: [],
-    order: requestOrder,
+const createMenu = (objeto) => ({
+  fetchMenu: objeto,
+  consumption: [],
+  order: requestOrder,
 });
 
-function requestOrder (request) {this.consumption.push(request)};
+function requestOrder(request) {this.consumption.push(request)};
 
 module.exports = createMenu;

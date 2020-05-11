2dac6d482bb8b43056e1b361411ccff46e806395
fogo5000 <lucaslucas2008@gmail.com>
2020-05-11 12:44:11 -0300

    Add requisition 8 1.0v

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,14 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let cont = 2; cont <= 3; cont += 1) {
+      myArray.push(cont);
     }
   }
   return myArray;
 };
+console.log(myCounter());
 
 module.exports = myCounter;

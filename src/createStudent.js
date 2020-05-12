506082c7d63ae887de22684ccafc626f623d0bab
binhus <eng.robsonwilli@gmail.com>
2020-05-12 16:08:27 -0300

    createStudent function

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (student) => {
+  const newStudent = {
+    name: student,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return newStudent;
+};
 
 module.exports = createStudent;

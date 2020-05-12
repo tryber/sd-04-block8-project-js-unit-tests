7387b8b31adfbde59e25b603add8a595177af4e6
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-05-12 17:47:43 -0300

    testes de productDetails.spec.js implementado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails('a', 'b'), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(productDetails('a', 'b')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (Object.keys(productDetails('a', 'b'))), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.ok(productDetails('a', 'b')[0] !== productDetails('a', 'b')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.ok(productDetails('a', 'b')[0].details.productId === 'a123' && productDetails('a', 'b')[1].details.productId === 'b123');
   });
 });

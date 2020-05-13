8c2b9f421903e8566da677452ea802d0ee6b33c9
Orlando <programadorlando@gmail.com>
2020-05-13 11:51:42 -0300

    comment assert.fail

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,12 +51,16 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
-    assert.ok(Object.is(obj1, createMenu(obj1).fetchMenu()) === true);  // Object.is method verifies if the values are the same value.
+    const menu = {
+      food: { coxinha: 3.9, sopa: 9.9 },
+      drink: { agua: 3.9, cerveja: 6.9 },
+    };
+    assert.deepStrictEqual(menu, createMenu(menu).fetchMenu);
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, verifique que 'objetoRetornado.fetchMenu' retorna um objeto cujas chaves são somente `food` e `drink`.

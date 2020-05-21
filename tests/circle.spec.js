6ce33d6519bbe85f6dc076e221389641ca85aa9e
lucasokazava <lucasokazava@gmail.com>
2020-05-21 13:38:06 -0300

    add circle.spec.js

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,6 +27,7 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const circ = circle(3);
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.

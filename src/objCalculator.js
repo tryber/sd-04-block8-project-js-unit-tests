450db3024909a9d2c33026f9556ec08aa11729b4
lucasokazava <lucasokazava@gmail.com>
2020-05-21 04:01:22 -0300

    fix cc(2) again in objCalculator.js

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,18 +20,10 @@
 */
 
 const calculator = {
-  add: (a, b) => {
-    return a + b;
-  },
-  mult: (a, b) => {
-    return a * b;
-  },
-  div: (a, b) => {
-    return Math.round(a / b);
-  },
-  sub: (a, b) => {
-    return a - b;
-  },
+  add: (a, b) => {return a + b},
+  mult: (a, b) => {return a * b},
+  div: (a, b) => {return Math.round(a / b)},
+  sub: (a, b) => {return a - b},
 };
 
 module.exports = calculator;
